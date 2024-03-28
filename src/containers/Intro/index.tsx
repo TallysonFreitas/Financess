4e8b74340e9c1dfc409b773889501123952b6749
Tallyson Freitas <tallyson.eu@outlook.com>
2024-03-28 11:55:08 -0300

    feat: cria container de introducao

---
+++ src/containers/Intro/index.tsx
@@ -0,0 +1,55 @@
+const Intro = () => {
+  return (
+    <section className="bg-dark">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <div className="text-center mb-5">
+              <h2 className="text-light fw-bold fs-1">Organize suas</h2>
+              <h2 className="fw-bold fs-1  text-primary">Financas</h2>
+            </div>
+            <p className="lead text-light fs-5 fw-light mb-5">
+              Organize sua vida financeira com facilidade! Bem-vindo ao nosso
+              site de finanças, sua ferramenta indispensável para alcançar
+              estabilidade e tranquilidade financeira. Sabemos que gerenciar
+              suas finanças pode ser desafiador, mas estamos aqui para
+              simplificar esse processo para você.
+            </p>
+
+            <p className="lead text-light fs-5 fw-light mb-5">
+              Com nosso site intuitivo e recursos abrangentes, você pode
+              acompanhar seus gastos, criar orçamentos personalizados e
+              visualizar suas metas financeiras de forma clara e acessível. Não
+              importa se você está economizando para uma viagem dos sonhos,
+              planejando sua aposentadoria ou simplesmente querendo controlar
+              melhor seus gastos diários, nós temos as ferramentas certas para
+              você.
+            </p>
+
+            <p className="lead text-light fs-5 fw-light mb-5">
+              Além disso, oferecemos conselhos e dicas financeiras úteis para
+              ajudá-lo a maximizar seu dinheiro e evitar armadilhas comuns.
+              Nosso objetivo é capacitá-lo a tomar decisões financeiras
+              informadas e alcançar seus objetivos sem estresse desnecessário.
+            </p>
+
+            <p className="lead text-light fs-5 fw-light mb-5">
+              Com a segurança e confiabilidade do nosso site, você pode ficar
+              tranquilo sabendo que suas informações estão protegidas enquanto
+              trabalha para melhorar sua situação financeira. Então, não espere
+              mais. Junte-se a nós hoje e comece a trilhar o caminho para uma
+              vida financeira mais organizada e próspera!
+            </p>
+
+            <a href="#" className="btn btn-light fw-semibold mt-5">
+              Fazer Simulacao
+            </a>
+          </div>
+        </div>
+        <div className="row py-5"></div>
+      </div>
+    </section>
+  )
+}
+
+export default Intro
