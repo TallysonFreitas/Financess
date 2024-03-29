14a5bb955f7d42f04089f5f467c51073acaa2414
Tallyson Freitas <tallyson.eu@outlook.com>
2024-03-29 11:39:03 -0300

    feat: corrige forms

--- src/containers/Intro/index.tsx
+++ src/containers/Intro/index.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
+import TituloGradrient from '../../components/TituloGradient'
+
 const Intro = () => {
+  const [valores, setValores] = useState({
+    salario: '',
+    despesas: '',
+    dividas: ''
+  })
+
   return (
-    <section className="bg-light-subtle">
+    <section className="bg-white">
       <div className="container">
         <div className="row">
           <div className="col-12 text-center">
             <div className="text-center mb-5">
               <h2 className="text-dark fw-bold fs-1">Organize your</h2>
-              <h2 className="fw-bold fs-1  text-primary">Personal finances</h2>
+              <h2 className="fw-bold">
+                <TituloGradrient>Personal finances</TituloGradrient>
+              </h2>
             </div>
             <p className="lead text-dark fs-4 fw-medium mb-5">
               Organize sua{' '}
@@ -60,9 +71,91 @@ const Intro = () => {
                 ></button>
               </div>
               <div className="modal-body">
-                <form></form>
+                <form>
+                  {/* Salario Input */}
+                  <div className="mb-3">
+                    <label htmlFor="salario" className="form-label">
+                      Salário
+                    </label>
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon1">
+                        R$
+                      </span>
+                      <input
+                        type="number"
+                        value={valores.salario}
+                        onChange={(e) => {
+                          setValores({
+                            ...valores,
+                            salario: e.target.value
+                          })
+                        }}
+                        className="form-control"
+                        placeholder="0,00"
+                        aria-label="salario"
+                        aria-describedby="basic-addon1"
+                        id="salario"
+                      />
+                    </div>
+                  </div>
+                  {/* Despesas Input */}
+                  <div className="mb-3">
+                    <label htmlFor="despesas" className="form-label">
+                      Despesas
+                    </label>
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon2">
+                        R$
+                      </span>
+                      <input
+                        type="number"
+                        value={valores.despesas}
+                        onChange={(e) => {
+                          setValores({
+                            ...valores,
+                            despesas: e.target.value
+                          })
+                        }}
+                        className="form-control"
+                        placeholder="0,00"
+                        aria-label="despesas"
+                        aria-describedby="basic-addon2"
+                        id="despesas"
+                      />
+                    </div>
+                  </div>
+                  {/* Dívidas Input */}
+                  <div className="mb-3">
+                    <label htmlFor="dividas" className="form-label">
+                      Dívidas
+                    </label>
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon3">
+                        R$
+                      </span>
+                      <input
+                        type="number"
+                        value={valores.dividas}
+                        onChange={(e) => {
+                          setValores({
+                            ...valores,
+                            dividas: e.target.value
+                          })
+                        }}
+                        className="form-control"
+                        placeholder="0,00"
+                        aria-label="dividas"
+                        aria-describedby="basic-addon3"
+                        id="dividas"
+                      />
+                    </div>
+                  </div>
+                </form>
               </div>
               <div className="modal-footer">
+                salario:{valores.salario}
+                despesas:{valores.despesas}
+                dividas:{valores.dividas}
                 <button type="button" className="btn btn-danger fw-semibold">
                   Simular
                 </button>
