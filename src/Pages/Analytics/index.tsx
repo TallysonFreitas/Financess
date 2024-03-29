88263e977b77b3b2c281dba3b6f8d146c91dc9f0
Tallyson Freitas <tallyson.eu@outlook.com>
2024-03-29 15:57:07 -0300

    feat: cria nova rota Analytics

---
+++ src/Pages/Analytics/index.tsx
@@ -0,0 +1,13 @@
+import Graphics from '../../containers/Graphics'
+import Header from '../../containers/Header'
+
+const Analytics = () => {
+  return (
+    <>
+      <Header />
+      <Graphics />
+    </>
+  )
+}
+
+export default Analytics
--- src/Pages/Rotas.tsx
+++ src/Pages/Rotas.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './Home'
+import Analytics from './Analytics'
 
-const Rotas = createBrowserRouter([{ path: '/', Component: Home }])
+const Rotas = createBrowserRouter([
+  { path: '/', Component: Home },
+  { path: '/analytics', Component: Analytics }
+])
 
 export default Rotas
