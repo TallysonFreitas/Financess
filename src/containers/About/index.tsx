e7627eec346906ea46b0a2bda069f2e8d726c83a
Tallyson Freitas <tallyson.eu@outlook.com>
2024-03-30 15:55:41 -0300

    feat: adiciona conteudo ao container About

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -8,99 +8,106 @@ const About = () => {
           <h1 className="fw-bold">
             <TituloGradrient>Financess</TituloGradrient>
           </h1>
-          <h2 className="fw-bold">Entenda um pouco sobre nós</h2>
+          <h2 className="fw-bold">understand a little about us</h2>
         </div>
         <div className="row">
           <div className="accordion" id="accordionPanelsStayOpenExample">
             <div className="accordion-item">
               <h2 className="accordion-header">
                 <button
-                  className="accordion-button"
+                  className="accordion-button fw-semibold fs-4"
                   type="button"
                   data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseOne"
+                  data-bs-target="#whoAccordion"
                   aria-expanded="true"
-                  aria-controls="panelsStayOpen-collapseOne"
+                  aria-controls="whoAccordion"
                 >
-                  Accordion Item #1
+                  Who are We?
                 </button>
               </h2>
               <div
-                id="panelsStayOpen-collapseOne"
+                id="whoAccordion"
                 className="accordion-collapse collapse show"
               >
                 <div className="accordion-body">
-                  <strong>This is the first item&apos;s accordion body.</strong>{' '}
-                  It is shown by default, until the collapse plugin adds the
-                  appropriate classes that we use to style each element. These
-                  classes control the overall appearance, as well as the showing
-                  and hiding via CSS transitions. You can modify any of this
-                  with custom CSS or overriding our default variables. It&apos;s
-                  also worth noting that just about any HTML can go within the{' '}
-                  <code>.accordion-body</code>, though the transition does limit
-                  overflow.
+                  Introducing Financess: Your Gateway to Financial Wellness
+                  Welcome to Financess, your ultimate destination for all things
+                  finance. Whether you&apos;re a seasoned investor or just
+                  starting your journey towards financial freedom, Financess has
+                  you covered. With a user-friendly interface and a plethora of
+                  resources, Financess empowers you to take control of your
+                  financial future. From budgeting tools to investment guides,
+                  our platform offers comprehensive solutions tailored to your
+                  needs. Join our community of like-minded individuals and gain
+                  access to expert advice, educational content, and interactive
+                  features designed to enhance your financial literacy.
+                  Don&apos;t let financial uncertainty hold you back. Visit
+                  Financess today and embark on the path to a brighter financial
+                  tomorrow.
                 </div>
               </div>
             </div>
             <div className="accordion-item">
               <h2 className="accordion-header">
                 <button
-                  className="accordion-button collapsed"
+                  className="accordion-button fw-semibold fs-4"
                   type="button"
                   data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="panelsStayOpen-collapseTwo"
+                  data-bs-target="#whatAccordion"
+                  aria-expanded="true"
+                  aria-controls="whatAccordion"
                 >
-                  Accordion Item #2
+                  What We do?
                 </button>
               </h2>
               <div
-                id="panelsStayOpen-collapseTwo"
-                className="accordion-collapse collapse"
+                id="whatAccordion"
+                className="accordion-collapse collapse show"
               >
                 <div className="accordion-body">
-                  <strong>
-                    This is the second item&apos;s accordion body.
-                  </strong>{' '}
-                  It is hidden by default, until the collapse plugin adds the
-                  appropriate classes that we use to style each element. These
-                  classes control the overall appearance, as well as the showing
-                  and hiding via CSS transitions. You can modify any of this
-                  with custom CSS or overriding our default variables. It&apos;s
-                  also worth noting that just about any HTML can go within the{' '}
-                  <code>.accordion-body</code>, though the transition does limit
-                  overflow.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ducimus mollitia ipsum ipsa eum odit architecto! Delectus quia
+                  eos ab cumque, maiores obcaecati nihil natus possimus
+                  voluptatibus, illum repellendus et harum doloribus doloremque.
+                  Neque ex saepe earum, necessitatibus aliquid quia fugit vero
+                  cupiditate officia amet illo in fugiat veniam unde provident a
+                  numquam asperiores fuga eos odio dolorum, tenetur non! Vitae
+                  modi voluptatibus rem harum in tempore corporis labore, sint
+                  laborum. Earum reprehenderit, facere perspiciatis unde quasi
+                  quae consequuntur accusamus illo.
                 </div>
               </div>
             </div>
             <div className="accordion-item">
               <h2 className="accordion-header">
                 <button
-                  className="accordion-button collapsed"
+                  className="accordion-button fw-semibold fs-4"
                   type="button"
                   data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseThree"
-                  aria-expanded="false"
-                  aria-controls="panelsStayOpen-collapseThree"
+                  data-bs-target="#trustAccodion"
+                  aria-expanded="true"
+                  aria-controls="trustAccodion"
                 >
-                  Accordion Item #3
+                  Can I trust in You?
                 </button>
               </h2>
               <div
-                id="panelsStayOpen-collapseThree"
-                className="accordion-collapse collapse"
+                id="trustAccodion"
+                className="accordion-collapse collapse show"
               >
                 <div className="accordion-body">
-                  <strong>This is the third item&apos;s accordion body.</strong>{' '}
-                  It is hidden by default, until the collapse plugin adds the
-                  appropriate classes that we use to style each element. These
-                  classes control the overall appearance, as well as the showing
-                  and hiding via CSS transitions. You can modify any of this
-                  with custom CSS or overriding our default variables. It&apos;s
-                  also worth noting that just about any HTML can go within the{' '}
-                  <code>.accordion-body</code>, though the transition does limit
-                  overflow.
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Eligendi enim iste culpa et nisi perspiciatis esse a, ad
+                  doloribus fuga corporis at doloremque deleniti, dolorem
+                  temporibus consequuntur nemo deserunt aperiam sit cum beatae
+                  voluptate veritatis vitae. Quidem doloremque rerum a omnis
+                  necessitatibus magnam quisquam. Enim consectetur nisi dolores
+                  iusto odio iste expedita quas repudiandae totam aut tenetur
+                  fuga consequatur quaerat corrupti perspiciatis mollitia, animi
+                  voluptate eius. Saepe tempore dolor voluptas quaerat ut, harum
+                  inventore, rem suscipit ipsum aliquam debitis quis tempora
+                  reiciendis dolorum incidunt sapiente doloribus ipsam quasi
+                  sint non?
                 </div>
               </div>
             </div>
